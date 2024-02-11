aebebb1031aa7af8c06e446ba729ded2b3eb73c2
s202153750 <s202153750@kfupm.edu.sa>
2024-02-11 13:07:28 +0300

    Last version with selection sort and insertion sort and their methods

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,4 +1,27 @@
 public class Sorter {
+
+    public static void selectionSort(int[] array) {
+
+        int min = 0;
+        for (int i = 0; i < array.length; i++) {
+            min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void sort2(int[] array) {
+
+        selectionSort(array); // calling the function inside sort function.
+    }
+
     public static void insertionSort(int[] arr) {
 
         int l = arr.length;
@@ -15,6 +38,8 @@ public class Sorter {
 
         }
     }
+
+
     public static void sort(int[] array) {
 
         insertionSort(array); // calling the function inside sort function.
@@ -26,10 +51,17 @@ public class Sorter {
 
         sort(array); // calling the sort
 
-        System.out.print("Sorted array is: ");
+        System.out.print("Sorted array is using insertion sort: ");
         for (int i = 0; i < array.length; ++i)
             System.out.print(" " + array[i]);
         System.out.println();
+
+
+
+        System.out.print("\nSorted array is using selection sort:\n ");
+        int[] array2 = {5,23,1,7,4,7,9};
+        sort2(array2);
+
     }
 
 }
