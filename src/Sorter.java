ed5daa056f3f740b0ada42c415640b13231f95c8
s202153750 <s202153750@kfupm.edu.sa>
2024-02-11 12:28:13 +0300

    version 1 with selection sort

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,4 +1,28 @@
 public class Sorter {
 
 
+
+
+
+    public void selectionSort(int[] arr) {
+
+        int min = 0;
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+            System.out.println(arr[i]);
+        }
+
+    }
+
+
+
+
 }
