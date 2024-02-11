0a20ead2fc9413c0425b419a78c2a208cde43d7a
s202159350 <s202159350@kfupm.edu.sa>
2024-02-11 12:34:30 +0300

    Version 1 with insertionSort().

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,4 +1,18 @@
 public class Sorter {
+    public static void insertionSort(int[] arr) {
 
+        int l = arr.length;
 
+        for (int i = 1; i < l; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+
+        }
+    }
 }
